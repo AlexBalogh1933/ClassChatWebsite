e9bef449db404c5cd4cc1b85d4f1405401ee8430
colton <daltonco@mail.uc.edu>
2023-11-06 20:45:02 -0500

    npm install parse
    install parse for back4app

---
+++ node_modules/parse/lib/react-native/isRevocableSession.js
@@ -0,0 +1,7 @@
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = isRevocableSession;
+function isRevocableSession(token) {
+  return token.indexOf('r:') > -1;
+}
